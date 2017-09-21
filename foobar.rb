4e29282036403d219b7511b16aeb86b5620bcc74
Anastasia Desyatnikov <adesyatnikov@berkeley.edu>
2017-09-20 18:12:50 -0700

    Complete Q1-4

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,8 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map!{|item| item.to_i + 2}
+    a.keep_if{|item| item.even? and item < 10}
+    return a.uniq.inject(:+)
   end
 end
-
-
